259be5ae926adf0fe779c87974460f9f6888d403
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-07-14 18:17:46 -0500

    imported all the needed Components from the components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
+
+import { createMarkup } from './helperFunctions'
+
+import Nav from './components/Nav'
+import Home from './components/Home'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
+import Badges from './components/Badges'
+import Footer from './components/Footer'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    )
-  }
+    constructor(props){
+        super(props)
+        this.state = { 
+            data: {}
+         }
+    }
+
+     fetchData() {
+         axios.get('http://localhost:8080/ResumeData.json')
+         .then(res => {
+             this.setState({
+                 data: res.data
+             })
+         })
+     }
+
+     componentDidMount() {
+         this.fetchData()
+     }
+
+    render() {
+        const { data } = this.state
+        
+        return (
+            <Fragment>
+                <Nav />
+                    <main style={{fontFamily: 'sans-serif', marginTop: '60px', overflow: 'hidden'}}>
+                    <Home 
+                    main={data.main} 
+                    social={data.social} />
+                    <About 
+                    about={data.about} />
+                    <Skills 
+                    skills={data.skills} />
+                    <Projects 
+                    projects={data.projects} />
+                    <Testimonials testimonials={data.testimonials} />
+                    <Badges />
+                </main>
+                <Footer social={data.social} />
+            </Fragment>
+        )
+    }
 }
+
 
 export default App
