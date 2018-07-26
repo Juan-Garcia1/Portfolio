4a3b740284ac09124eb1d62cfd1f2d93e75d7ea0
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-07-26 13:40:25 -0500

    removed unnecessary space

--- src/App.js
+++ src/App.js
@@ -1,60 +1,59 @@
-import React, { Component, Fragment } from 'react'
-import axios from 'axios'
+import React, { Component, Fragment } from "react";
+import axios from "axios";
 
-import Nav from './components/Nav'
-import Home from './components/Home'
-import About from './components/About'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Testimonials from './components/Testimonials'
-import Badges from './components/Badges'
-import Footer from './components/Footer'
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
+import Badges from "./components/Badges";
+import Footer from "./components/Footer";
 
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = { 
-            data: {}
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    };
+  }
 
-     fetchData() {
-         axios.get('ResumeData.json')
-         .then(res => {
-             this.setState({
-                 data: res.data
-             })
-         })
-     }
+  fetchData() {
+    axios.get("ResumeData.json").then(res => {
+      this.setState({
+        data: res.data
+      });
+    });
+  }
 
-     componentDidMount() {
-         this.fetchData()
-     }
+  componentDidMount() {
+    this.fetchData();
+  }
 
-    render() {
-        const { data } = this.state
-        
-        return (
-            <Fragment>
-                <Nav />
-                    <main style={{fontFamily: 'sans-serif', marginTop: '60px', overflow: 'hidden'}}>
-                    <Home 
-                    main={data.main} 
-                    social={data.social} />
-                    <About 
-                    about={data.about} />
-                    <Skills 
-                    skills={data.skills} />
-                    <Projects 
-                    projects={data.projects} />
-                    <Testimonials testimonials={data.testimonials} />
-                    <Badges />
-                </main>
-                <Footer social={data.social} />
-            </Fragment>
-        )
-    }
+  render() {
+    const { data } = this.state;
+
+    return (
+      <Fragment>
+        <Nav />
+        <main
+          style={{
+            fontFamily: "sans-serif",
+            marginTop: "60px",
+            overflow: "hidden"
+          }}
+        >
+          <Home main={data.main} social={data.social} />
+          <About about={data.about} />
+          <Skills skills={data.skills} />
+          <Projects projects={data.projects} />
+          <Testimonials testimonials={data.testimonials} />
+          <Badges />
+        </main>
+        <Footer social={data.social} />
+      </Fragment>
+    );
+  }
 }
 
-
-export default App
+export default App;
