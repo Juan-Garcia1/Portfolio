b435f5146fa907fdb3ba8938b799cf8848979560
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-08-16 13:59:46 -0500

    removed unnecessary comments

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 // import styled from 'styled-components'
 
-import { Container } from '../Styles/Container'
-import { NavBar
-    // , HamburgerIcon, SideNav, SideNavItem
- } from '../Styles/Nav'
+import { Container } from "../Styles/Container"
+import {
+  NavBar
+  // , HamburgerIcon, SideNav, SideNavItem
+} from "../Styles/Nav"
 
-import { primaryColor } from '../Styles/Global'
+import { primaryColor } from "../Styles/Global"
 
 const Flex = Container.extend`
-    display: flex;
-    justify-content: space-between;
-    // align-items: center;
-    align-items: flex-end;
-    h1 {
-        margin: 0;
-        font-size: 18px;
-        font-weight: 300;
-        font-family: sans-serif;
-    }
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  h1 {
+    margin: 0;
+    font-size: 18px;
+    font-weight: 300;
+    font-family: sans-serif;
+  }
 `
 
 class Nav extends Component {
-    // constructor(props) {
-        // super(props)
-        // this.state = {
-        //     isOpen: false
-        // }
-        // this.handleToggle = this.handleToggle.bind(this)
-    // }
+  // constructor(props) {
+  // super(props)
+  // this.state = {
+  //     isOpen: false
+  // }
+  // this.handleToggle = this.handleToggle.bind(this)
+  // }
 
-    // handleToggle() {
-    //     const { isOpen } = this.state
-    //     this.setState({
-    //         isOpen: !isOpen
-    //     })
-    // }
-    render() {
-        // const { isOpen } = this.state
-        // const toggleSideNav = isOpen ? 'open-sidenav' : ''
-        // const activeHamburgerIcon = isOpen ? 'active' : ''
-        return(
-            <NavBar>
-                <Flex>
-                    <h1 style={{ color: primaryColor }}>Juan Garcia</h1>
-                    {/* <HamburgerIcon
+  // handleToggle() {
+  //     const { isOpen } = this.state
+  //     this.setState({
+  //         isOpen: !isOpen
+  //     })
+  // }
+  render() {
+    // const { isOpen } = this.state
+    // const toggleSideNav = isOpen ? 'open-sidenav' : ''
+    // const activeHamburgerIcon = isOpen ? 'active' : ''
+    return (
+      <NavBar>
+        <Flex>
+          <h1 style={{ color: primaryColor }}>Juan Garcia</h1>
+          {/* <HamburgerIcon
                         onClick={this.handleToggle}
                         className={activeHamburgerIcon}>
                         <i className="fa fa-bars"></i>
@@ -56,10 +56,10 @@ class Nav extends Component {
                         <SideNavItem>Projects</SideNavItem>
                         <SideNavItem>Badges</SideNavItem>
                     </SideNav> */}
-                </Flex>
-            </NavBar>
-        )
-    }
+        </Flex>
+      </NavBar>
+    )
+  }
 }
 
-export default Nav;
+export default Nav
