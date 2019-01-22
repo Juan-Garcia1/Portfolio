a1902045014b60196a20d5ccf7f9041dabe50232
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-01-22 12:24:56 -0600

    Updates

---
+++ precache-manifest.6bfb393b43474d1704e1ec1c1450e193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "151c53c1f6f7f75a6c16193935206d9b",
+    "url": "/portfolio/static/media/chicago.151c53c1.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "14577571b8161fd9d2e6",
+    "url": "/portfolio/static/js/main.14577571.chunk.js"
+  },
+  {
+    "revision": "8c8b05aa2f2648dbab53",
+    "url": "/portfolio/static/js/1.8c8b05aa.chunk.js"
+  },
+  {
+    "revision": "c6e4acdc4a983deea0cfc128f04f8496",
+    "url": "/portfolio/index.html"
+  }
+];
