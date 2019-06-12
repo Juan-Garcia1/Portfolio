f0d3c82f086efac8efac8079f549a3e3f9890612
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-06-12 17:28:05 -0500

    updated navController duration

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
   // HOME PAGE
 
   // highlight link when user scrolls to its corresponding section
-  let navController = new ScrollMagic.Controller({ globalSceneOptions: { duration: "100%" } });
+  let navController = new ScrollMagic.Controller({
+    globalSceneOptions: { duration: "70%" }
+  });
 
   new ScrollMagic.Scene({ triggerElement: "#about" })
     .setClassToggle(".nav-link-1", "active-nav-link")
-    .addTo(navController)
+    .addTo(navController);
 
   new ScrollMagic.Scene({ triggerElement: "#projects" })
     .setClassToggle(".nav-link-2", "active-nav-link")
-    .addTo(navController)
+    .addTo(navController);
 
   new ScrollMagic.Scene({ triggerElement: "#experience" })
     .setClassToggle(".nav-link-3", "active-nav-link")
-    .addTo(navController)
-
+    .addTo(navController);
 
   // SINGLE PAGE PROJECTS SLIDER
-  $('.slider-single').slick({
+  $(".slider-single").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
@@ -27,16 +27,15 @@ $(document).ready(function () {
     pauseOnHover: false,
     autoplay: true,
     autoplaySpeed: 6000,
-    asNavFor: '.slider-nav'
+    asNavFor: ".slider-nav"
   });
-  $('.slider-nav').slick({
+  $(".slider-nav").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    asNavFor: '.slider-single',
+    asNavFor: ".slider-single",
     dots: false,
     arrows: false,
     centerMode: true,
     focusOnSelect: true
   });
-
-})
+});
