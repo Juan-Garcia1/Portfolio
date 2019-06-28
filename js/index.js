46dd07821a3fe1091bc6f5bbd9235802e49354b6
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-06-28 11:20:45 -0500

    added loading screen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-document
-  .querySelector(".view-projects-btn")
-  .addEventListener("click", function() {
-    TweenLite.to(window, 0.75, { scrollTo: "#projects" });
+document.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("load", function() {
+    document.querySelector(".loader").classList.add("finished-loading");
+    document.querySelector("body").style.overflow = "auto";
+    // scroll to projects section
+    document
+      .querySelector(".view-projects-btn")
+      .addEventListener("click", function() {
+        TweenLite.to(window, 0.75, { scrollTo: "#projects" });
+      });
   });
+});
