a77b2b090a2c28013186f33e36674c20b3082d1e
Juan Garcia <juan_garcia_1995@hotmail.com>
2019-08-01 14:21:05 -0500

    add email work on projects section

--- js/index.js
+++ js/index.js
@@ -8,5 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
       .addEventListener("click", function() {
         TweenLite.to(window, 0.75, { scrollTo: "#projects" });
       });
+    const webBtn = document.querySelector("#web-btn");
+    const emailBtn = document.querySelector("#email-btn");
+    const projectsList = document.querySelector("#projects-list");
+    const emailsList = document.querySelector("#emails-list");
+
+    // when web button is clicked, display list of web projects only
+    webBtn.addEventListener("click", function() {
+      emailBtn.classList.remove("active");
+      this.classList.add("active");
+      emailsList.style.display = "none";
+      projectsList.style.display = "";
+    });
+    // when email button is clicked, display list of emails projects only
+    emailBtn.addEventListener("click", function() {
+      webBtn.classList.remove("active");
+      this.classList.add("active");
+      emailsList.style.display = "";
+      projectsList.style.display = "none";
+    });
   });
 });
