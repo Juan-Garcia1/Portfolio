cf20d8cf078e5e1139f99f13865b58d8b3b9f371
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2019-10-04 10:22:48 -0500

    added landing page projects

--- js/index.js
+++ js/index.js
@@ -9,22 +9,37 @@ document.addEventListener("DOMContentLoaded", function() {
         TweenLite.to(window, 0.75, { scrollTo: "#projects" });
       });
     const webBtn = document.querySelector("#web-btn");
+    const landersBtn = document.querySelector("#landers-btn");
     const emailBtn = document.querySelector("#email-btn");
     const projectsList = document.querySelector("#projects-list");
+    const landersList = document.querySelector("#landers-list");
     const emailsList = document.querySelector("#emails-list");
 
     // when web button is clicked, display list of web projects only
     webBtn.addEventListener("click", function() {
       emailBtn.classList.remove("active");
+      landersBtn.classList.remove("active");
       this.classList.add("active");
       emailsList.style.display = "none";
+      landersList.style.display = "none";
       projectsList.style.display = "";
     });
     // when email button is clicked, display list of emails projects only
     emailBtn.addEventListener("click", function() {
       webBtn.classList.remove("active");
+      landersBtn.classList.remove("active");
       this.classList.add("active");
       emailsList.style.display = "block";
+      projectsList.style.display = "none";
+      landersList.style.display = "none";
+    });
+    // when lander button is clicked, display list of landing projects only
+    landersBtn.addEventListener("click", function() {
+      webBtn.classList.remove("active");
+      emailBtn.classList.remove("active");
+      this.classList.add("active");
+      landersList.style.display = "block";
+      emailsList.style.display = "none";
       projectsList.style.display = "none";
     });
   });
