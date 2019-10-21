b88f62d7d88bd259432de9a090dc2d007a14078a
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2019-10-21 15:54:31 -0500

    added list of wordpress projects

--- js/index.js
+++ js/index.js
@@ -11,36 +11,55 @@ document.addEventListener("DOMContentLoaded", function() {
     const webBtn = document.querySelector("#web-btn");
     const landersBtn = document.querySelector("#landers-btn");
     const emailBtn = document.querySelector("#email-btn");
+    const wpBtn = document.querySelector("#wordpress-btn");
     const projectsList = document.querySelector("#projects-list");
     const landersList = document.querySelector("#landers-list");
     const emailsList = document.querySelector("#emails-list");
+    const wpList = document.querySelector("#wordpress-list");
 
     // when web button is clicked, display list of web projects only
     webBtn.addEventListener("click", function() {
       emailBtn.classList.remove("active");
       landersBtn.classList.remove("active");
+      wpBtn.classList.remove("active");
       this.classList.add("active");
       emailsList.style.display = "none";
       landersList.style.display = "none";
+      wpList.style.display = "none";
       projectsList.style.display = "";
     });
     // when email button is clicked, display list of emails projects only
     emailBtn.addEventListener("click", function() {
       webBtn.classList.remove("active");
       landersBtn.classList.remove("active");
+      wpBtn.classList.remove("active");
       this.classList.add("active");
       emailsList.style.display = "block";
       projectsList.style.display = "none";
       landersList.style.display = "none";
+      wpList.style.display = "none";
     });
     // when lander button is clicked, display list of landing projects only
     landersBtn.addEventListener("click", function() {
       webBtn.classList.remove("active");
       emailBtn.classList.remove("active");
+      wpBtn.classList.remove("active");
       this.classList.add("active");
       landersList.style.display = "block";
       emailsList.style.display = "none";
       projectsList.style.display = "none";
+      wpList.style.display = "none";
+    });
+    // when wp button is clicked, display wp projects only
+    wpBtn.addEventListener("click", function() {
+      webBtn.classList.remove("active");
+      landersBtn.classList.remove("active");
+      emailBtn.classList.remove("active");
+      this.classList.add("active");
+      projectsList.style.display = "none";
+      landersList.style.display = "none";
+      emailsList.style.display = "none";
+      wpList.style.display = "block";
     });
   });
 });
